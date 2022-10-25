db394ca843962b8ce07bf14872f628d5a9e51363
DimPap99 <dimtrispap1999@gmail.com>
2022-10-25 11:56:37 +0300

    Finished Chapter 17

--- Entities/RequestFeatures.cs
+++ Entities/RequestFeatures.cs
@@ -25,7 +25,11 @@ namespace Entities
     }
     public class EmployeeParameters : RequestParameters
     {
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set; } = int.MaxValue;
+        public bool ValidAgeRange => MaxAge > MinAge;
     }
+
     public class MetaData
     {
         public int CurrentPage { get; set; }
